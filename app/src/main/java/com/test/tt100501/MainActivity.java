348d89fe7553fe37fe0625c9f5a7733d01d2b86b
Shaojung <shaojung@gmail.com>
2016-10-05 10:45:15 +0800

    加入網頁的 Action View

--- app/src/main/java/com/test/tt100501/MainActivity.java
+++ app/src/main/java/com/test/tt100501/MainActivity.java
@@ -1,7 +1,12 @@
 package com.test.tt100501;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Uri uri = Uri.parse("http://tw.yahoo.com");
+        Intent it = new Intent(Intent.ACTION_VIEW, uri);
+        // it.setAction(Intent.ACTION_VIEW);
+        // it.setAction("android.intent.action.VIEW");
+        startActivity(it);
+
+
     }
 }
